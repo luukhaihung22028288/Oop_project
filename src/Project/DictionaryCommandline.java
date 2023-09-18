b1eac58ae6ef190eb1fd1344a23b6ac15b475eef
luukhaihung <125259299+luukhaihung22028288@users.noreply.github.com>
2023-09-18 21:04:48 +0700

    ading API Translate

--- src/Project/DictionaryCommandline.java
+++ src/Project/DictionaryCommandline.java
@@ -1,16 +1,18 @@
 package Project;
+import java.io.IOException;
 import java.util.Scanner;
 public class DictionaryCommandline {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         DictionaryManagement d = new DictionaryManagement();
-        d.dictionaryinsertFromFile();
-        d.showAllWords();
-        d.insertFromCommandline();
-       // Scanner input=new Scanner(System.in);
-       // String s=input.nextLine();
+       // d.dictionaryinsertFromFile();
+        //d.showAllWords();
+       // d.insertFromCommandline();
+        Scanner input=new Scanner(System.in);
+        String s=input.nextLine();
 
-        d.dictionaryExportToFile();
-
+       // d.dictionaryExportToFile();
+       d.dictionaryTrans(s);
     }
+
 }
--- src/Project/DictionaryManagement.java
+++ src/Project/DictionaryManagement.java
@@ -1,11 +1,15 @@
 package Project;
 
-import java.util.*;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.*;
 
 
 public class DictionaryManagement {
@@ -166,5 +170,29 @@ public class DictionaryManagement {
         }
         System.out.println("There is no word");
     }
+
+    private static String translate(String langFrom, String langTo, String text) throws IOException {
+        // INSERT YOU URL HERE
+        String urlStr = "https://script.google.com/macros/s/AKfycbz5XOgKRoSF8p45rgNUyMpvEIyiY7wTrLJypLVsuQDm7PbCap-y9yu8N0tu_EVeNOhU/exec" +
+                "?q=" + URLEncoder.encode(text, "UTF-8") +
+                "&target=" + langTo +
+                "&source=" + langFrom;
+        URL url = new URL(urlStr);
+        StringBuilder response = new StringBuilder();
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
+    public void dictionaryTrans(String text) throws IOException {
+        System.out.println("Translated text: " + translate("en", "vi", text));
+    }
+
+
 
 }
